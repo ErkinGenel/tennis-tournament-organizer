8cc77daca19dc24b6a7e17d2f7f28e1b06114115
ErkinGenel <165898299+ErkinGenel@users.noreply.github.com>
2026-06-06 11:15:58 +0200

    Update base path in Vite configuration

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/tennis-organizer/', // <-- ADD THIS LINE (Replace with your exact repo name)
+  base: '/tennis-tournament-organizer/', // <-- ADD THIS LINE (Replace with your exact repo name)
 })
